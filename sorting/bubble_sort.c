ffe75e5a03de300e054b7d13a556641918192c7f
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-09-06 17:11:09 -0600

    formatting changes

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 // Pretty print the resulting array
 void print_array(int arr[], int arrLength) {
-  for(int i = 0; i < arrLength; ++i) {
-    if(i == 0) {
-      printf("{ %d", arr[i]);
-    } else {
-      printf(", %d", arr[i]);
+    for(int i = 0; i < arrLength; ++i) {
+        if(i == 0) {
+            printf("{ %d", arr[i]);
+        } else {
+            printf(", %d", arr[i]);
+        }
     }
-  }
-  printf(" } \n");
+    printf(" } \n");
 }
 
 // Swap the two values
@@ -21,20 +22,26 @@ void swap(int *x, int *y) {
 
 // Sort the array
 void bubble_sort(int arr[], int arrLength) {
+    bool isSorted;
     for (int i = arrLength; i > 0; --i) {
-      for (int j = 0; j < i - 1; ++j) {
-        if (arr[j] > arr[j + 1]) {
-          swap(&arr[j], &arr[j + 1]);
+        isSorted = true; // Assume the Array is sorted
+        for (int j = 0; j < i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                isSorted = false;
+            }
         }
-      }
+        if (isSorted) {
+            break;
+        }    
     }
 }
 
 int main(void) {
-  int arr[4] = { 37, 45, 29, 8 };
-  int arrLength = sizeof(arr) / sizeof(int);
-  print_array(arr, arrLength);
-  bubble_sort(arr, arrLength);
-  print_array(arr, arrLength);
-  return 0;
+    int arr[4] = { 37, 45, 29, 8 };
+    int arrLength = sizeof(arr) / sizeof(int);
+    print_array(arr, arrLength);
+    bubble_sort(arr, arrLength);
+    print_array(arr, arrLength);
+    return 0;
 }
